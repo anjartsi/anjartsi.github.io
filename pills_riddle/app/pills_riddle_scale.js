6c08148cef9989a2652b35cf429ef616bd968d28
Armen Ourfalian <aourfalian@gmail.com>
2015-05-24 11:06:26 -0700

    Final pills_riddle added

--- pills_riddle/app/pills_riddle_scale.js
+++ pills_riddle/app/pills_riddle_scale.js
@@ -2,9 +2,8 @@ var scaleUses=3;
 var difference=0;
 var scaleOne=0;
 var scaleTwo=0;
-var scaleOnePills=[];
-var scaleTwoPills=[];
-var emptyBoxes=[];
+var pillsOnScale=[[],[]];
+var emptyContBoxes=[];
 var difference;
 
 
@@ -15,67 +14,77 @@ var scaleTwoFeedback = document.getElementById('scaleTwoFeedback');
 var scaleComparison = document.getElementById('scaleComparison');
 var weighButton = document.getElementById('weighButton');
 var clearScale = document.getElementById('clearScale');
+var clearLeft = document.getElementById('clearLeft');
+var clearRight = document.getElementById('clearRight');
+var answer = document.getElementById('answer');
 
-var checkScalePills = function(){
-	scaleOnePills.length=0;
-	scaleTwoPills.length=0;
-	emptyBoxes.length=0;
-	for (var i = 1; i < numPills+1; i++) {
-		if(allPills[i].group==1){
-			scaleOnePills.push(i);
-			emptyBoxes.push(i);
-		}
-		else if (allPills[i].group==2){
-			scaleTwoPills.push(i);
-			emptyBoxes.push(i);
+var weigh = function(cont) {
+	var weight = 0;
+	for (var i=1;i<allPillBoxes[cont].length;i++){
+		var pillNum=allPillBoxes[cont][i].pill;
+		if(pillNum>0){
+			weight+= allPills[pillNum].weight;
+			pillsOnScale[cont-1].push(pillNum);
 		}
 	}
+	return weight;
 }
 
 var calculateDifference = function() {
 	difference = 0;
-	scaleOne = 0;
-	scaleTwo = 0;
-	checkScalePills();
-	for(var i=0;i<scaleOnePills.length;i++){
-		scaleOne+=allPills[scaleOnePills[i]].weight;
-	}
-	for(var i=0;i<scaleTwoPills.length;i++){
-		scaleTwo+=allPills[scaleTwoPills[i]].weight;
-	}
-	return difference = scaleOne-scaleTwo;
+	pillsOnScale[0].length=0;
+	pillsOnScale[1].length=0;
+	return difference = weigh(1)-weigh(2);
 }
 
-var printFeedback = function() {
-	// Print scaleOneFeedback and ScaleTwoFeedback. 
-	// Each pill is followed by a comma (except the last pill in each group)
-	for(var i=0;i<scaleOnePills.length;i++){
-		scaleOneFeedback.innerHTML+='Pill_'+scaleOnePills[i];
-		if(i+1<scaleOnePills.length){scaleOneFeedback.innerHTML+=', '}
+// Print which pills were weighed
+var printRecords = function(){
+	for(var i=0;i<pillsOnScale[0].length;i++){
+		scaleOneFeedback.innerHTML+='Pill_'+pillsOnScale[0][i];
+		if(i+1<pillsOnScale[0].length){scaleOneFeedback.innerHTML+=', '}
 	}
-	for(var i=0;i<scaleTwoPills.length;i++){
-		scaleTwoFeedback.innerHTML+='Pill_'+scaleTwoPills[i];
-		if(i+1<scaleTwoPills.length){scaleTwoFeedback.innerHTML+=', '}
+	for(var i=0;i<pillsOnScale[1].length;i++){
+		scaleTwoFeedback.innerHTML+='Pill_'+pillsOnScale[1][i];
+		if(i+1<pillsOnScale[1].length){scaleTwoFeedback.innerHTML+=', '}
 	}
-
-	// Print the Comparison
-	if(difference>0){scaleComparison.innerHTML+='weighs MORE THAN';	}
-	else if(difference>0){scaleComparison.innerHTML+='weighs LESS THAN';	}
-	else{scaleComparison.innerHTML+='weighs THE SAME AS';}
-
-	// Add a white line to scaleOneFeedback, scaleTwoFeedback, and scaleComparison
 	if(scaleUses>0){
 		scaleOneFeedback.innerHTML+='<hr>';
-		scaleTwoFeedback.innerHTML+='<hr>';
-		scaleComparison.innerHTML+='<hr>';
+		scaleTwoFeedback.innerHTML+='<hr>';		
 	}
+}
 
-	//Decrement the weighButton output
-	weighButton.innerHTML='Weigh (&times '+scaleUses+')';
-	if(scaleUses==0){
-		weighButton.disabled=true;
-		weighButton.style.color='#777777';
-	}
+// Print which side of the scale was heavier
+var printComparison = function() {
+	if(difference>0){scaleComparison.innerHTML+='is HEAVIER THAN';	}
+		else if(difference<0){scaleComparison.innerHTML+='is LIGHTER THAN';	}
+		else{scaleComparison.innerHTML+='weighs THE SAME AS';}
+	if(scaleUses>0){scaleComparison.innerHTML+='<hr>';}
+}
+
+// What happens when the scale is used 3 times
+var scaleDead = function() {
+	// Disable the WeighButton
+	weighButton.disabled=true;
+	weighButton.style.color='#777777';
+
+	// Enable the answer div
+	removeClass(answer,'disabled');
+	addClass(answer,'enabled');
+
+	// Create pillBox in answer div
+	pillBoxCreator(answer,1);
+	document.getElementById('pillBox_5_1').addEventListener('dragenter', pbDragEnterListener);
+	document.getElementById('pillBox_5_1').addEventListener('dragover', pbDragEnterListener);
+	document.getElementById('pillBox_5_1').addEventListener('dragleave', pbDragLeaveListener);
+	document.getElementById('pillBox_5_1').addEventListener('drop', pbDropListener);
+	document.getElementById('pillBox_5_1').addEventListener('drop', function(e){
+		var data = e.dataTransfer.getData('application/pill_number');
+		data = data.split(',');
+		var parentEl = document.getElementById(data[0]);
+		var pillNum = parseInt(data[1],10);
+		if (allPills[pillNum].isPoison){alert ('Well Done');}
+		else{alert("You're Dead!")}
+	});
 }
 
 
@@ -83,28 +92,42 @@ var useScale = function() {
 	if(scaleUses>0){
 		scaleUses--;
 		calculateDifference();
-		printFeedback();
+		printRecords();
+		printComparison();
 	}
+	if(scaleUses==0){
+		scaleDead();
+	}
+	// Print scaleUses on the Weigh Button
+	weighButton.innerHTML='Weigh (&times '+scaleUses+')';
+}
+
+var clearLeftScale = function() {
+	weigh(1);
+	for(var i=0;i<pillsOnScale[0].length;i++){
+		movePill(pillsOnScale[0][i],pillBoxID(3,1))
+	}
+	pillsOnScale[0].length=0;
+}
+
+var clearRightScale = function() {
+	weigh(2);
+	for(var i=0;i<pillsOnScale[1].length;i++){
+		movePill(pillsOnScale[1][i],pillBoxID(4,1))
+	}
+	pillsOnScale[1].length=0;
 }
 
 var clearScales = function() {
-	checkScalePills();
-
-	var usedPills=scaleOnePills.concat(scaleTwoPills);
-	if(usedPills.length>0){
-		for(var i=0;i<usedPills.length;i++){
-			movePill(usedPills[i],pillBoxID('Cont',emptyBoxes[i]));
-		}
-	}
+	clearRightScale();
+	clearLeftScale();
 }
 
 var buttonEventListeners = function() {
 	weighButton.addEventListener('mousedown',useScale);
 	clearScale.addEventListener('mousedown',clearScales);
-
+	clearLeft.addEventListener('mousedown',clearLeftScale);
+	clearRight.addEventListener('mousedown',clearRightScale);
 }
-
-
-
 
 buttonEventListeners();
