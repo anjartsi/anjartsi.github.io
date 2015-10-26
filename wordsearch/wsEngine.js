4af1e5e5bbadc7a7148e9d4558cf7e805bd1ee45
Armen Ourfalian <aourfalian@gmail.com>
2015-10-26 01:37:11 -0700

    Day 2
    well it's actually day 3

--- wordsearch/wsEngine.js
+++ wordsearch/wsEngine.js
@@ -3,6 +3,28 @@ Initialize the word search
 
 This file uses the Letter object defined in letter.js
 *************************************************/
+//problem size
+var prbSize = 15;
+var fontSize = 25; // Need a better name for this variable
+
+// wordList
+// List of words to search for, all upper case
+// wordList starts out as a string but is converted to an array
+var wordList = "hello world foo bar lorem ipsum armen ourfalian";
+wordList = wordList.toUpperCase();
+wordList = wordList.split(' ');
+
+// charList
+// All the letters that make up the words
+var charList = [];
+for(var i = 0, l = wordList.length; i < l; i++) {
+  for(var j = 0,ll = wordList[i].length; j < ll; j++) {
+    if (charList.indexOf(wordList[i][j]) < 0) {
+      charList.push(wordList[i][j]);
+    }
+  }
+}
+
 
 // canvas stuff
 var wordsearch = document.getElementById('wordsearch');
